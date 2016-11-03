c800ea94e14c0dd85c91ae95992740614b9f1789
LONG <longt2407@gmail.com>
2016-11-03 22:45:10 +0700

    Add Ghost2.cs

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,17 +6,11 @@ public class Ghost : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                Debug.Log(mapManager[i, j]);
+        Debug.Log(mapManager[0, 0]); 
     }
-	
+   
 	// Update is called once per frame
 	void Update () {
-        // con ma muốn tinh1 đường đi thi phải biết hết cái map phai k 
-        // neu ma brick hay vat can minh khong viet script lay toa do thi dung vay :D 
-        // cái ô góc dưới trái là tọa độ 00, giống như oxy, muốn xem ô nào đó là game object gì thì
-        // get trong cái map manager ra
         
-	}
+    }
 }
