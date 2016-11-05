4038f6f4c2c443570b8f841daff7457e4785ed7c
LONG <longt2407@gmail.com>
2016-11-05 23:50:46 +0700

    Viet ham bat su kien player thay doi vi tri

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Ghost : MonoBehaviour, MapManagerListener {
     public MapManager mapManager;
     private Rigidbody2D rigidBody;
+    public GameObject player;
 
     // ghost attribute
     public float heart;
@@ -17,6 +18,7 @@ public class Ghost : MonoBehaviour, MapManagerListener {
 
     private bool ready;
     private bool changed;
+    private MapLocation player_last_position;
 
     private List<MapLocation> path = new List<MapLocation>();
     private Vector3 destination;
@@ -26,7 +28,7 @@ public class Ghost : MonoBehaviour, MapManagerListener {
     // Check if ghost is ready to move to next position int path
     private bool check_ready()
     {
-        Debug.Log("Check if ghost ready : " + Vector3.Distance(transform.position, destination));
+        // Debug.Log("Check if ghost ready : " + Vector3.Distance(transform.position, destination));
         if (Vector3.Distance(transform.position, destination) <= PIVOT)
         {
             ready = true;
@@ -50,15 +52,32 @@ public class Ghost : MonoBehaviour, MapManagerListener {
         }
 
         // implement floyd warshall algorithm
-        MapLocation temp = new MapLocation(7, 11);
-        path.Add(temp);
-        temp = new MapLocation(9, 11);
-        path.Add(temp);
+        //MapLocation temp = new MapLocation(7, 11);
+        //path.Add(temp);
+        //temp = new MapLocation(9, 11);
+        //path.Add(temp);
+    }
+
+    // on flame collision handler
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "flame")
+        {
+            Debug.Log("Ghost mat mau do dinh flame");
+
+            heart -= 1;
+            if (heart == 0)
+            {
+                Debug.Log("Ghost die");
+
+                Destroy(this.gameObject);
+            }   
+        }
     }
 
     private void move()
     {
-        Debug.Log("Ghost is moving");
+        // Debug.Log("Ghost is moving");
 
         Vector3 temp = destination - transform.position;
         Vector2 move = new Vector2(
@@ -74,7 +93,9 @@ public class Ghost : MonoBehaviour, MapManagerListener {
     }
 
 	public void onMapReady() {
-        Debug.Log("Init ghost value");
+        // Debug.Log("Init ghost value");
+        player = mapManager.getPlayer();
+        player_last_position = MapManager.vector3ToMapLocation(player.transform.position);
 
         booms = mapManager.getBooms();
         flames = mapManager.getFlame();
@@ -87,19 +108,36 @@ public class Ghost : MonoBehaviour, MapManagerListener {
 
     public void onMapChanged()
     {
+        // Check if obstructions on map change
+        Debug.Log("Map changed");
         booms = mapManager.getBooms();
         flames = mapManager.getFlame();
         changed = true;
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
+        // Check if player move to new position
+        MapLocation player_current_position = MapManager.vector3ToMapLocation(player.transform.position);
+
+        if (player_current_position.X != player_last_position.X
+            || player_current_position.Y != player_last_position.Y)
+        {
+            Debug.Log("Player move to new position");
+
+            player_last_position = player_current_position;
+            changed = true;
+        }
+
         // Find new path when map changed
         if (changed == true)
         {
-            changed = false;
+            Debug.Log("Map thay doi");
             if (check_ready() == true)
             {
+                Debug.Log("Ma da di chuyen xong, tim duong moi");
+
+                changed = false;
                 pathFinding();
             }
         }
@@ -107,7 +145,7 @@ public class Ghost : MonoBehaviour, MapManagerListener {
         // Check if ghost go to old destination successfully
         if (check_ready() == true)
         {
-            Debug.Log("Ghost ready to move to new location");
+            // Debug.Log("Ghost ready to move to new location");
             if (path.Count != 0)
             {
                 MapLocation temp = path[0];
@@ -120,12 +158,12 @@ public class Ghost : MonoBehaviour, MapManagerListener {
             }
             else
             {
-                // di random 1 o xung quanh
+                // Dung im khong di chuyen
             }
         }
         else
         {
-            Debug.Log("Ghost is moving to new location");
+            // Debug.Log("Ghost is moving to new location");
             move();
         }
     }
