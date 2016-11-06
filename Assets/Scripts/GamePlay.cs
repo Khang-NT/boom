8eb6fcfd99fbe2e78a2930d8eee2774d2f29c729
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-07 00:05:51 +0700

    [UPDATE] Update GamePlay: load/save data, high score logic

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class GamePlay {
     private static GamePlay sInstance;
+	private static readonly string FILE_NAME = "gameplay.data";
 
     public static GamePlay getInstance()
     {
@@ -19,14 +21,78 @@ public class GamePlay {
     int boardId;
     string playerName;
 	int highScore;
-	int highScorePlayerName;
+	string highScorePlayerName;
 
     protected GamePlay()
     {
-        // load high score from file
-        boardId = 1;
-        score = 0;
-        heart = 3;
-        playerName = "bla";
+		loadDataFromFile ();
+		reset ();
     }
+
+	public string PlayerName {
+		get {
+			return playerName;
+		}
+
+		set {
+			this.playerName = value;
+		}
+	}
+
+	public int Heart {
+		get {
+			return heart;
+		}
+
+		set {
+			this.heart = value;
+		}
+	}
+
+	public int BoardId {
+		get {
+			return boardId;
+		}
+
+		set {
+			this.boardId = value;
+		}
+	}
+
+	public int Score {
+		get {
+			return score;
+		}
+
+		set {
+			this.score = value;
+			if (score >= highScore) {
+				this.highScore = score;
+				this.highScorePlayerName = playerName;
+				saveDataToFile ();
+			}
+		}
+	}
+
+	public void reset() {
+		boardId = 1;
+		score = 0;
+		heart = 3;
+	}
+
+	protected void loadDataFromFile() {
+		highScore = 0;
+		highScorePlayerName = "";
+		if (File.Exists (FILE_NAME)) {
+			string[] lines = File.ReadAllLines (FILE_NAME);
+			if (lines.Length == 2) {
+				highScore = int.Parse (lines [0]);
+				highScorePlayerName = lines [1];
+			}
+		}
+	}
+
+	protected void saveDataToFile() {
+		File.WriteAllLines (FILE_NAME, new string[]{ highScore.ToString (), highScorePlayerName });
+	}
 }
