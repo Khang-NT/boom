739800c49dd46100c54ea47996d4a3f5fbcf1076
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-08 01:44:45 +0700

    [ADD] Add script WelcomeScreenController

---
+++ Assets/Scripts/WelcomeScreenController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WelcomeScreenController : MonoBehaviour {
+
+	public void startGame() {
+		string heroName = GameObject.Find ("HeroName").GetComponent<Text>().text;
+		if (heroName != null && heroName.Length > 0) {
+			GamePlay.getInstance ().reset ();
+			GamePlay.getInstance ().PlayerName = heroName;
+			SceneManager.LoadScene (1); 
+		}
+	}
+}
