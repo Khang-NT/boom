44b2d0422ec92f12fd7c851c98e74fff5a4163d1
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-08 02:33:04 +0700

    [ADD] Add script `EndGameController`

---
+++ Assets/Scripts/EndGameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameController : MonoBehaviour {
+
+
+
+	void Start () {
+		GamePlay gamePlay = GamePlay.getInstance ();
+		GameObject.Find("GameResult").GetComponent<Text>().text = gamePlay.Win ? "Win!!!" : "GameOver";
+		GameObject.Find ("Score").GetComponent<Text> ().text = gamePlay.Score.ToString ();
+		GameObject.Find ("PlayerName").GetComponent<Text> ().text = gamePlay.PlayerName;
+		GameObject.Find ("HighScore").SetActive (gamePlay.Score == gamePlay.HighScore);
+	}
+	
+	public void newGame() {
+		SceneManager.LoadScene (0);
+	}
+}
