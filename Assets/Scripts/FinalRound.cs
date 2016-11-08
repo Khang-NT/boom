b6e9eaafeb71cb0039725eaaaa2e93f9c403668d
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-09 00:03:06 +0700

    [ADD] Add `Round4`

---
+++ Assets/Scripts/FinalRound.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalRound : MonoBehaviour, MapManagerListener {
+
+	public MapManager mapManager;
+
+	bool isOpened = false;
+
+	// Use this for initialization
+	void Start () {
+		mapManager.addListener (this);
+	}
+
+	public void onMapReady() {
+
+	}
+
+	public void onMapChanged() {
+		if (!isOpened && mapManager.getGhosts ().Count == 1) {
+			isOpened = true;
+			foreach (var brick in GameObject.FindGameObjectsWithTag("cage")) {
+				mapManager.removeBrick (MapManager.getMapLocation(brick));
+				Destroy (brick);
+			}
+			Destroy(GameObject.Find ("BossCage"));
+		}
+	}
+
+	public void onPlayerDied() {
+
+	}
+
+	public void onAllGhostsDied() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
